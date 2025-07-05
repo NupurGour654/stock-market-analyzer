469d8697e279c8f1963637bd5616d463f5f32120
NupurGour654 <155764854+NupurGour654@users.noreply.github.com>
2025-07-05 23:04:28 +0530

    feat: added date input and passed to fetch logic

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ from stock_utils import fetch_stock_data, plot_closing_price, save_data_to_csv
 def main():
     symbol = input("Enter stock ticker symbol (e.g., AAPL): ").strip().upper()
     print(f"Fetching data for {symbol}...")
-    data = fetch_stock_data(symbol)
+    #data = fetch_stock_data(symbol)
+    start = input("Enter start date (YYYY-MM-DD): ")
+    end = input("Enter end date (YYYY-MM-DD): ")
+    data = fetch_stock_data(symbol, start, end)
+
 
     if data is not None:
         plot_closing_price(data, symbol)
